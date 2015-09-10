870c89649032480587bdb03ae31f4eecf21eebf7
lord63 <lord63.j@gmail.com>
2015-09-10 09:00:16 +0800

    Use color configurations from config file

--- tldr/parser.py
+++ tldr/parser.py
@@ -5,19 +5,24 @@ from __future__ import absolute_import
 
 import click
 
+from tldr.config import get_config
+
 
 def parse_page(page):
     """Parse the command man page."""
+    colors = get_config()['colors']
     with open(page) as f:
         lines = f.readlines()
     for line in lines:
         if line.startswith('#'):
             continue
         elif line.startswith('>'):
-            click.secho(line.replace('>', ' '), fg='blue', nl=False)
+            click.secho(line.replace('>', ' '), fg=colors['description'],
+                        nl=False)
         elif line.startswith('-'):
-            click.secho(line, fg='green', nl=False)
+            click.secho(line, fg=colors['usage'], nl=False)
         elif line.startswith('`'):
-            click.secho('  ' + line.replace('`', ''), fg='cyan', nl=False)
+            click.secho('  ' + line.replace('`', ''), fg=colors['command'],
+                        nl=False)
         else:
             click.secho(line, nl=False)
