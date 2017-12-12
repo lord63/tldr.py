3d37670e81889f0c6607a5c402878742923f9279
Nicolas Clerc <kernicPanel@nclerc.fr>
2017-12-13 00:16:54 +0100

    rework bash autocompletion

--- autocompletion/tldr.bash
+++ autocompletion/tldr.bash
@@ -1,9 +1,17 @@
-function _tldr_autocomplete {
-    commands=$(tldr list)
-    COMPREPLY=()
+_tldr_completion() {
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _TLDR_COMPLETE=complete $1 ) )
+
     if [ $COMP_CWORD = 2 ]; then
-        COMPREPLY=(`compgen -W "$commands" -- $2`)
+        command_name=${COMP_WORDS[COMP_CWORD-1]}
+        if [[ "$command_name" == "find" ]] || [[ "$command_name" == "locate" ]]; then
+            pages=$(tldr list)
+            COMPREPLY=(`compgen -W "$pages" -- $2`)
+        fi
     fi
+
+    return 0
 }
 
-complete -F _tldr_autocomplete tldr
+complete -F _tldr_completion -o default tldr;
